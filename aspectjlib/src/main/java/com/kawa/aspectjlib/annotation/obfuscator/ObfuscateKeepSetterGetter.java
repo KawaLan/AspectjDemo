616e96f1feb5078d38cf977a186e0c6b1a68c88b
zhenlin.liang <zhenlin.liang@g-emall.com>
2019-01-08 13:44:47 +0800

    补充错误修改

--- aspectjlib/src/main/java/com/kawa/aspectjlib/annotation/obfuscator/ObfuscateKeepSetterGetter.java
+++ aspectjlib/src/main/java/com/kawa/aspectjlib/annotation/obfuscator/ObfuscateKeepSetterGetter.java
@@ -8,9 +8,12 @@ import java.lang.annotation.Inherited;
  *  Created:         Kawa on 2019/1/8 11:24.
  *  E-mail:          958129971@qq.com
  *  Desc:       本类的set/get方法 不被混淆
- *      -keepclassmembers class * {
- *      @com.kawa.aspectjlib.annotation.obfuscator.ObfuscateKeepThisField * ;
- *      }
+ *     -keepclassmembers @com.kawa.aspectjlib.annotation.obfuscator.ObfuscateKeepSetterGetter class * {
+ *       void set*(***);
+ *       boolean is*();
+ *       *** get*();
+ *  }
+ *
  * </pre> 
  ****/
 @Inherited
--- aspectjlib/src/main/java/com/kawa/aspectjlib/annotation/obfuscator/ObfuscateKeepThisField.java
+++ aspectjlib/src/main/java/com/kawa/aspectjlib/annotation/obfuscator/ObfuscateKeepThisField.java
@@ -1,17 +1,19 @@
 package com.kawa.aspectjlib.annotation.obfuscator;
 
+import java.lang.annotation.Inherited;
+
 /****
  * <pre>
  *  Project_Name:    Bookkeeping
  *  Created:         Kawa on 2019/1/8 11:25.
  *  E-mail:          958129971@qq.com
  *  Desc:
- *  -keepclassmembers @com.kawa.aspectjlib.annotation.obfuscator.ObfuscateKeepSetterGetter class * {
- *     void set*(***);
- *     boolean is*();
- *     *** get*();
- * }
+ -keepclassmembers class * {
+ *      @com.kawa.aspectjlib.annotation.obfuscator.ObfuscateKeepThisField * ;
+ *      }
  * </pre> 
  ****/
-public class ObfuscateKeepThisField {
+@Inherited
+public @interface ObfuscateKeepThisField {
+
 }
